4c2ee68d3bf384b879b2d71761ae6fe63be363c4
BlakeCG <cristopherguerrero1994@gmail.com>
2021-07-09 16:48:07 -0400

    app and sql database are now talking to eachother

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT * FROM song-history')
+db.execute('SELECT * FROM songhistory')
     .then(result => {
         console.log(result)
     })
